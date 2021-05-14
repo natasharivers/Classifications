5d30591e654d6dc5058df873356654aee876d879
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-05-14 16:13:27 -0500

    create prepare for iris

---
+++ prepare.py
@@ -0,0 +1,10 @@
+def prep_iris(df):
+    '''
+    accepts the untrasformed iris data
+    returns: the data with cleaning operations performed on it 
+    '''
+    df = df.drop(columns= (['species_id', 'measurement_id']))
+    df = df.rename(columns = {'species_name': 'species'})
+    dummy_name = pd.get_dummies(df[['species']], dummy_na = False, drop_first=[True])
+    df = pd.concat([df, dummy_name], axis=1)
+    return df
