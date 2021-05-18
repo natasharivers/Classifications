22b4fc048912d0173709c5bc67d0e596eebea212
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-05-18 10:23:47 -0500

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -11,14 +11,15 @@ def prep_iris(df):
 
 
 
-def train_validate_test_split(df, seed=123):
-    train_and_validate, test = train_test_split(
-        df, test_size=0.2, random_state=seed, stratify=df.survived
-    )
-    train, validate = train_test_split(
-        train_and_validate,
-        test_size=0.3,
-        random_state=seed,
-        stratify=train_and_validate.survived,
-    )
+def titanic_split(df):
+    '''
+    This function performs split on titanic data, stratify survived.
+    Returns train, validate, and test dfs.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=123, 
+                                        stratify=df.survived)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                   random_state=123, 
+                                   stratify=train_validate.survived)
     return train, validate, test
