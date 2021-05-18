fddb72e67bbc51dbedd152c42fded9bf0de2d9a0
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-05-18 10:11:02 -0500

    add train test split

--- prepare.py
+++ prepare.py
@@ -9,3 +9,16 @@ def prep_iris(df):
     df = pd.concat([df, dummy_name], axis=1)
     return df
 
+
+
+def train_validate_test_split(df, seed=123):
+    train_and_validate, test = train_test_split(
+        df, test_size=0.2, random_state=seed, stratify=df.survived
+    )
+    train, validate = train_test_split(
+        train_and_validate,
+        test_size=0.3,
+        random_state=seed,
+        stratify=train_and_validate.survived,
+    )
+    return train, validate, test
