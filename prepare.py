418ea1370e6a022c90340be634d2b7bc608941a5
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-05-18 11:15:25 -0500

    add to prepare

--- prepare.py
+++ prepare.py
@@ -22,7 +22,10 @@ def titanic_split(df):
     return train, validate, test
 
 
+
+
 def train_validate_test_split(df, seed=123):
     train_and_validate, test = train_test_split(df, test_size=0.2, random_state=seed, stratify=df.survived)
-    train, validate = train_test_split(train_and_validate,test_size=0.3, random_state=seed, stratify=train_and_validate.survived,)
+    train, validate = train_test_split(train_and_validate,test_size=0.3, random_state=seed, stratify=train_and_validate.survived)
+    
     return train, validate, test
