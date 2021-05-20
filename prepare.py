98ff77fbaf4344f7a1f0bf5bd6ce9ca205b1a170
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-05-20 15:56:45 -0500

    fix prepare

--- prepare.py
+++ prepare.py
@@ -1,18 +1,14 @@
-from pydataset import data
-import seaborn as sns
 import pandas as pd
 import numpy as np
-import os
-from env import host, user, password, get_db_url
 
 def prep_iris(df):
-    '''
-    takes in a dataframe of the iris dataset as it is acquired and returns a cleaned dataframe
-    arguments: df: a pandas DataFrame with the expected feature names and columns
-    return: clean_df: a dataframe with the cleaning operations performed on it
-    '''
-    df = df.drop(columns=['species_id', 'measurement_id'])
-    df = df.rename(columns={'species_name': 'species'})
-    dummy_df = pd.get_dummies(df[['species']], dummy_na=False, drop_first=[True])
-    df = pd.concat([df, dummy_df], axis=1)
-    return df
+    """
+    This function takes the iris data set (from aquire.py) as input.
+    And outputs a clean dataset ready for modeling.
+    Clean in this case means: species and measurement id columns removed, species_name changed to species and seperated 
+    into dummy variables
+    """
+    cleaned_df = df.drop(columns = ['species_id', 'measurement_id'])
+    cleaned_df = cleaned_df.rename(columns= {'species_name': 'species'})
+    cleaned_df = pd.get_dummies(data=cleaned_df, columns= ['species'], drop_first = True)
+    return cleaned_df
