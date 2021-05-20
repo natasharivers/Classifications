58cf776780f6f2a92ce1c70f463de42bf7a461cb
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-05-20 15:19:06 -0500

    add to prepare

--- prepare.py
+++ prepare.py
@@ -29,3 +29,31 @@ def train_validate_test_split(df, seed=123):
     train, validate = train_test_split(train_and_validate,test_size=0.3, random_state=seed, stratify=train_and_validate.survived)
     
     return train, validate, test
+
+
+def prep_titanic (df):
+    '''
+    This function take in the titanic data acquired by get_titanic_data,
+    Returns prepped train, validate, and test dfs with embarked dummy vars,
+    deck dropped, and the mean of age imputed for Null values.
+    '''
+    
+    # drop rows where embarked/embark town are null values
+    df = df[~df.embarked.isnull()]
+    
+    # encode embarked using dummy columns
+    titanic_dummies = pd.get_dummies(df.embarked, drop_first=True)
+    
+    # join dummy columns back to df
+    df = pd.concat([df, titanic_dummies], axis=1)
+    
+    # drop the deck column
+    df = df.drop(columns='deck')
+    
+    # split data into train, validate, test dfs
+    train, validate, test = titanic_split(df)
+    
+    # impute mean of age into null values in age column
+    train, validate, test = impute_mean_age(train, validate, test)
+    
+    return train, validate, test
